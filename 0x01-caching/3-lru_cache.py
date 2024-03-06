362ee5cdd883d273a691aa64ec152edd9fd72616
ewurapoms <mzpomaa35@gmail.com>
2024-03-06 04:42:06 -0800

    LRU Caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Basecaching Module"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """implements an LRU caching system"""
+
+    def __init__(self):
+        """initializer"""
+        super().__init__()
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            del self.order[key]
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key, _ = self.order.popitem(last=False)
+            del self.cache_data[lru_key]
+            print(f"DISCARD: {lru_key}")
+        self.cache_data[key] = item
+        self.order[key] = None
+
+    def get(self, key):
+        """Get an item by key"""
+        if key is None or key not in self.cache_data:
+            return None
+        self.order.move_to_end(key)
+        return self.cache_data.get(key)
